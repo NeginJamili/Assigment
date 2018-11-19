6ad2a623932e8d9742d46dfd96d4595ff68d3b66
Negin <njamili3@yahoo.com>
2018-11-19 17:45:14 +0100

    Q2

--- Assign1.R
+++ Assign1.R
@@ -5,3 +5,17 @@ forbes <- read_csv("forbes.csv")
 forbes$rank <- parse_number(forbes$rank)
 forbes$net_worth <- parse_number(forbes$net_worth)
 forbes$age <- parse_number(forbes$age)
+
+# Q2 ----------------------------------------------------------------------
+
+# Those 3-digit numbers are in million, so they must be removed 
+forbes <- forbes %>%
+  filter (nchar(trunc(net_worth)) <= 2)
+
+# Although other codes (such as the below one) are also applicable
+forbes <- forbes %>%
+  filter (net_worth < 954)
+
+# We could also separate the net_worth into 2 columns (before using parse_number)
+# by separate(forbes, into = c("net_worth", "letter"), sep = " ")
+# and then use parse_number and filter those with B letters
