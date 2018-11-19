78a632b105551289901a02faa7fe5f7a89c56644
Negin <njamili3@yahoo.com>
2018-11-19 17:51:54 +0100

    Q3

--- Assign1.R
+++ Assign1.R
@@ -19,3 +19,40 @@ forbes <- forbes %>%
 # We could also separate the net_worth into 2 columns (before using parse_number)
 # by separate(forbes, into = c("net_worth", "letter"), sep = " ")
 # and then use parse_number and filter those with B letters
+
+# Q3 ----------------------------------------------------------------------
+
+ggplot (forbes, aes (x = age, y = net_worth)) +
+  geom_point()
+
+# Since the data includes some numbers much larger than the majority, 
+# it is useful to use logarithmic scales in this chart 
+ggplot (forbes, aes (x = age, y = log(net_worth))) +
+  geom_point()
+
+# For improving the plot, it's better to ommit the rows with no specific age
+ggplot(data = filter(forbes, age > 0), aes(x = age, y = log(net_worth))) +
+  geom_point()
+
+# In spite of the negligibe increase of the worth observed in older people,
+# in order to provide a better plot, I am going to manipulate the data 
+# and categorize the age of people into 8 groups
+
+forbes <- forbes %>%
+  filter (age > 0) %>%
+  mutate (age_group = age %/% 10 - 1) 
+
+forbes$age_group <- factor(forbes$age_group,
+                           levels = c(1:8),
+                           labels = c("20-30", "30-40", "40-50", "50-60", "60-70", 
+                                      "70-80", "80-90", ">90"))
+
+ggplot(data = forbes, mapping = aes(x = age_group, y = log(net_worth), fill = age_group)) +
+  geom_boxplot(show.legend = FALSE) +
+  xlab('Age Group') +
+  ylab('Log of net worth') 
+
+# Considering the plot, now we can observe that older people have more wealth
+# in average, although the larger amounts are possessed by 70-80 years old people.
+
+
