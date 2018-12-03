29e1cb55111d5214253d0e5e2e2516073afe593e
Negin <njamili3@yahoo.com>
2018-12-03 11:19:59 +0100

    Q2

--- assignment-2.R
+++ assignment-2.R
@@ -54,36 +54,47 @@ get_jane_austen_data <- function(){
 get_jane_austen_data ()
 
 # extract_possible_names 
-
-austen_text1 <- austen_text
-# to find number of words in each row, to be able to separate into columns
-austen_textCop <- mutate (austen_text1, NoWords = 
-                            str_count(austen_text1$text, '\\w+'))
-# find the maximum number of words to know the number of new columns to be created
-maxWords <- max(austen_text1$NoWords) + 1
-# first I didn't add 1, then I noticed this error:
-# Additional pieces discarded in 7 rows. I found out that the reason is that " is counted as one word 
-# in sentences that start with ". So I added 1 to resolve this error
-
-# name of the new columns
-mynames = c(paste("word",1:maxWords,sep=""))
-# separating all words of a sentence into several columns 
-Austen_Sep <- separate(austen_text1, "text", into = mynames, remove = TRUE)
-# using the previous function to gather all the words in 1 column
-tidy_df <- function(data, column_prefix = "var"){
-  data %>%
-    gather(starts_with(column_prefix), key = "variable", value = "value") %>% 
-    select(variable, value, everything())
+#' Title
+#'
+#' @param data 
+#' @param nameCol: is the column in which the text is given
+#' @param idCol: is the column in which id of each text is provided
+#' @maxWords is created to find number of words in each row, 
+#' to be able to separate into given number of columns
+#' @newColNames is the new of new columns
+#' the function then separates all words of a sentence into several columns
+#' and uses the previous function to gather all the words in 1 column
+#' 
+#' The col names are then adjusted and new unique ID is assigned to the words
+#' @return a table with 3 columns
+#' @export
+#'
+#' @examples
+extract_possible_names <- function(data, nameCol, idCol) {
+  data <- mutate (data, NoWords = 
+                    str_count(data[[nameCol]], '\\w+'))
+  
+  maxWords <- max(data$NoWords) + 1
+  # first I didn't add 1, then I noticed this error:
+  # Additional pieces discarded in 7 rows. I found out that the reason is that " is counted as one word 
+  # in sentences that start with ". So I added 1 to resolve this error
+  
+  newColNames = c(paste("word",1:maxWords,sep=""))
+  data <- data %>%
+    separate(text, into = newColNames, remove = TRUE)
+  data <- tidy_df(data, "word")
+  data <- data %>% 
+    filter(value > 0 & str_detect(value,"^[A-Z]."))
+  #  . is added to remove one-letter words such as "A" or "I" from the list
+  data <- data %>% 
+    rename(text_id = id, name = value) %>% 
+    mutate(id = rownames(data)) %>% 
+    select(id, text_id, name)
 }
-Austen_Sep <- tidy_df(Austen_Sep, "word")
-# to filter those with capital first letter (. to removw "A" or "I" from the list)
-Austen_Sep <- filter(Austen_Sep, value > 0 & str_detect(value,"^[A-Z]."))
-#Adjusting the column names
-Austen_Sep = rename(Austen_Sep, text_id = id, name = value)
-# defining unique id
-Austen_Sep <- mutate(Austen_Sep, id = rownames(Austen_Sep))
 
-Austen_Sep <- select(Austen_Sep, id, text_id, name)
+# To check the function:
+austen_text1 <- austen_text
+austenWords <- extract_possible_names(austen_text1, "text", "id")
 
 # Question 3 ------------------------------------------------------------------------------------------------------
 austen_word_freqs <- readRDS("austen_word_freqs.Rds")
